bbaf644cde32555022b50f45503b2950480fe30b
Dorothy Thurston <dorothy.thur@gmail.com>
2015-03-02 18:02:33 -0500

    Delete color feedback. Add text feedback through comparisons.

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ $(document).ready(function () {
     event.preventDefault();
     $('form').show( 300 );
     $('#new-game').hide( 300 );
-    $('p').text("guess a number between 1 and 50");
+    $('p').text("guess a number between 1 and 100");
 
     var guessList = [];
-    var secretNum = Math.floor((Math.random() * 50) + 1);
+    var maxGuess = 100
+    var secretNum = Math.floor((Math.random() * maxGuess) + 1);
 
     $('form').submit(function(event){
       event.preventDefault();
+
       var newGuess = parseInt($('input').val());
 
       var inputGuessed = function(num,array) {
@@ -20,67 +22,45 @@ $(document).ready(function () {
         }
       };
 
-      var colorFeedback = function(secretNum, newGuess) {
-        var difference = Math.abs(secretNum - newGuess);
-
-        if (difference <= 50 && difference > 40) {
-          $('.main-container').css("background-color", "light-blue");
-        }
-        else if (difference <= 40  && difference > 30 ) {
-          $('.main-container').css("background-color", "blue");
-        }
-        else if (difference <= 30  && difference > 20 ) {
-          $('.main-container').css("background-color", "navy");
-        }
-        else if (difference <= 20  && difference > 10 ) {
-          $('.main-container').css("background-color", "purple");
-        }
-        else if (difference <= 10  && difference > 5 ) {
-          $('.main-container').css("background-color", "magenta");
-        }
-        else if (difference <= 5  && difference > 0 ) {
-          $('.main-container').css("background-color", "red");
-        }
-        else {
-          $('.main-container').css("background-color", "green");
-        }
-      };
-
-
-      var compareCloseness = function(secretNum, newGuess) {
+      var compareCloseness = function() {
         var lastGuess = guessList[(guessList.length-1)];
 
-        if (lastGuess) {
+        if (guessList.length < 1) {
+          $('ul').prepend("<li>nope. it's not " + newGuess + "</li>");
+        }
+        else if (lastGuess) {
           if (inputGuessed (newGuess , guessList)) {
             $('ul').prepend("<li>you already guessed " + newGuess + "</li>");
           }
           else if (Math.abs(secretNum - newGuess) < Math.abs(secretNum - lastGuess)) {
-              $('ul').prepend("<li>" + newGuess + " is warmer than " + lastGuess + "</li>");
+              $('ul').prepend("<li>" + newGuess + " is <span class='warm'>warmer</span> than " + lastGuess + "</li>");
           }
           else {
-            $('ul').prepend("<li>" + newGuess + " is colder than " + lastGuess + "</li>");
+            $('ul').prepend("<li>" + newGuess + " is <span class='cold'>colder</span> than " + lastGuess + "</li>");
           }
         }
       };
 
-      var displayResults = function (newGuess, guessList, secretNum) {
+      var displayResults = function () {
         if (newGuess === secretNum) {
             $('p').text("You got it!");
-            $('.main-container').css("background-color", "green");
             $('form').hide( 300 );
             $('#new-game').show( 300 );
             $('h3').text("");
             $('ul').text("");
             $('#user-input').val("");
+
+            while(guessList.length > 0) {
+              guessList.pop();
+            }
         }
         else {
-            colorFeedback(secretNum, newGuess);
             compareCloseness(secretNum, newGuess);
             guessList.push(newGuess);
         }
       };
 
-      displayResults(newGuess, guessList, secretNum);
+      displayResults();
     });
   });
 });
